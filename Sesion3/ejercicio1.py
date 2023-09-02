fe3efe4c687d92e31137e9d4097dba23501c6d90
jadg1308 <jadg13@hotmail.com>
2023-09-01 21:29:47 -0600

    Sesion  2 uso de funciones basicas

---
+++ Sesion3/ejercicio1.py
@@ -0,0 +1,25 @@
+"""Funciones int y eval"""
+num = "1"
+num = int(num)
+type(num)
+x = int(num)
+x
+type(x)
+y = eval(num)
+y
+a = "13.99"
+a
+b = eval(a)
+type(b)
+
+numero = int(input("Dime un # "))
+type(numero)
+
+numR = eval(input("Dime un # "))
+type(numR)
+
+numR2 = float(input("Dime un numero: "))
+type(numR2)
+numR2
+
+
---
+++ Sesion3/ejercicio2.py
@@ -0,0 +1,47 @@
+#Funcion de redondeo.
+num = -15.2565
+round(num, 3)
+
+#Funcion de Valor Absoluto
+print(abs(num))
+
+#funcion max y min
+a, b, c, d = 4, 2, 9, 3
+max(a, b, c, d)
+min(a, b, c, d)
+print(a, b, c, d)
+
+#Funcion potencia
+x, y, z = 3, 5500, 1 
+pow(x, y)
+
+#fomato o funcion format
+import os
+os.system("cls")
+x = 2563
+y = 5454545
+print(f"{format(x, '*>9')} \n{format(y, '*>9')}")
+
+#Funciones de texto
+print("Hola", "mundo")
+print("Hola", "mundo", sep='-')
+print("Hola", "mundo", sep=' ')
+print("Hola", "mundo", sep='*'*5)
+print("Hola", "mundo", sep=' jadg1308 '*5)
+print("Hola", "mundo", "Enrique", sep=' ')
+
+nombre = "Cesar"
+edad = 20
+print(nombre, edad, sep = '-'*3)
+print(nombre, edad, sep = '-'*3, end='')
+print(nombre, edad, sep = '-'*3, end=': ')
+
+
+#Str
+x = 7
+type(x)
+y = str(x)
+type(y)
+
+
+

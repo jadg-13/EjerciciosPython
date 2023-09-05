f54ae4d9501eefc9484ce2edb4051035fa94ed84
jadg1308 <jadg13@hotmail.com>
2023-09-04 19:56:49 -0600

    Ejercicio finalizado de la sesion 4

--- Sesion4/ejemplo1.py
+++ Sesion4/ejemplo1.py
@@ -1,27 +1,40 @@
 import os
-import math
+from math import *
 
 def calcAreaCirc(radio):
-    return round(math.pi * pow(radio, 2), 4)
+    return round(pi * pow(radio, 2), 4)
 
 def menu():
-    message = """1. Calcular el area de un circulo
+    op = 0
+    while(op!=4):
+        os.system("cls")
+        print("Sesión 04/09/2023")
+        
+        message = """1. Calcular el area de un circulo
 2. Calcular el cubo de un numero
 3. Calcular la raiz cuadrada de un numero
+4. Salir
 Digita tu opcion: """
-    print(message)
-    op = int(input())
-    if(op == 1):
-        r = float(input("Dime el radio: "))
-        print(f"El area circulo es {calcAreaCirc(r)}")
-    elif(op == 2):
-        n = int(input("Dime un número: "))
-        print(f"El cubo es : {pow(n, 3)}")
+        print(message)
+        op = int(input())
+        if(op == 1):
+            r = float(input("Dime el radio: "))
+            print(f"El area circulo es {calcAreaCirc(r)}")
+        elif(op == 2):
+            n = int(input("Dime un número: "))
+            print(f"El cubo es : {pow(n, 3)}")
+        elif(op == 3):
+            n = int(input("Dime un número: "))
+            print(f"La raiz cuadrada es: {sqrt(n)}")
+        elif(op==4):
+            print("Adios...")
+        else:
+            print("Opción invalida...")
+        os.system("pause")
 
 def main():
-    os.system("cls")
-    print("Sesión 04/09/2023")
-    menu()
+    
+    #menu()
 
 if __name__== '__main__':
     main()

19b425ec5427ccaaa8365dc8ea3c8782b5bf08f7
jadg1308 <jadg13@hotmail.com>
2023-09-18 23:57:27 -0600

    [update] Ejercicio de permutación de una palabra

---
+++ permutacion/permutar.py
@@ -0,0 +1,17 @@
+import itertools
+from os import system
+
+def permutaciones(palabra):
+    letras = list(palabra)
+    todas_permutaciones = list(itertools.permutations(letras))
+    i = 0
+    for permutacion in todas_permutaciones:
+        palabra_permutada = ''.join(permutacion)
+        print(f"Combinación: {str(i):>6}: {palabra_permutada}")
+        i+=1
+
+# Permutar
+system("cls||clear")
+
+palabra = input("Escribe una palabra: ")
+permutaciones(palabra)

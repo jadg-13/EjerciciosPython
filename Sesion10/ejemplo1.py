caa911f947475de4b47cf615243d5979375c44f2
jadg1308 <jadg13@hotmail.com>
2023-10-03 19:36:36 -0600

    Sesión 10

---
+++ Sesion10/ejemplo1.py
@@ -0,0 +1,75 @@
+# Arreglo de numeros 
+# Mostrar los numeros pares
+# Mostrar las suma de los impares
+# Mostar todos los elementos de la lista.
+# Buscar un elemento en la lista
+# Eliminar un valor
+
+import os 
+
+listNumbers = list()
+
+def addNum():
+    num = int(input("Dime un numero: "))
+    listNumbers.append(num)
+
+def showPairs():
+    os.system("cls||clear")
+    print("Mostrar numeros de numeros pares")
+    for number in listNumbers:
+        if number % 2 ==0:
+            print(number)
+    os.system("pause")
+
+def showSumUnPairs():
+    suma = 0
+    for num in listNumbers:
+        if num%2 != 0:
+            suma += num
+    print(f"Las Suma de los impares es: {suma}")
+    os.system("pause")
+
+def showList():
+    print(listNumbers)
+    os.system("pause")
+
+def searchNumber(number):
+    if number in listNumbers:
+        print(f"Se encontro {number}")
+    else:
+        print(f"No se encontro {number}")
+
+def menu():
+    print("""1. Agregar 
+2. Mostrar Pares
+3. Mostrar sumas impares
+4. Mostrar lista
+5. Buscar numero
+6. Salir""")
+    option = int(input("Digita la opcion: "))
+    if(option == 1):
+        addNum()
+    elif(option == 2):
+        showPairs()   
+    elif(option == 3):
+        showSumUnPairs()
+    elif(option == 4):
+        showList()
+    elif(option ==5):
+        number = int(input("Dime un numero: "))
+        searchNumber(number=number)
+        os.system("pause")
+    elif(option == 6):
+        print("Adios...")
+    else:
+        print("Opción existe")
+    return option
+    
+def main():
+    op = 0
+    while op != 6:
+        os.system("cls")
+        op = menu()
+
+main()
+
